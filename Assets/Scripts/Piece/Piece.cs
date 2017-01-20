2ab99457abf1c89bb81bb75b25f6161dd49e0dac
Aidan <aidan@digitgaming.com>
2017-01-20 21:49:10 +0000

    missed pieces

---
+++ Assets/Scripts/Piece/Piece.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+
+    #region private fields
+    private int[] _position = new int[2];
+    #endregion
+
+    #region MonoBehaviour utilities
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    #endregion
+}
