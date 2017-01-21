51b8540f371986432a4c46fdb6774e27f18ecd86
Aidan <aidan@digitgaming.com>
2017-01-21 11:21:46 +0000

    new scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PieceColour
+{
+    BLACK = 0,
+    WHITE = 1
+}
+
+public class GameManager : MonoBehaviour
+{
+
+    public PieceColour turn;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.turn = PieceColour.WHITE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
