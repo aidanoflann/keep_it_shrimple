4b22b13f6548562adfe03ef3cae62b2f61588b4c
Aidan <aidan@digitgaming.com>
2017-01-21 00:25:44 +0000

    transform is directly accessible dummy

--- Assets/Scripts/Piece/Piece.cs
+++ Assets/Scripts/Piece/Piece.cs
@@ -6,20 +6,18 @@ public class Piece : MonoBehaviour {
     #region private fields
     private int[] _position = new int[2];
     private Board _board;
-    private GameObject _gameObject;
     #endregion
 
     public void Place(Vector3 position)
     // place the piece - snapping to nearest board position.
     {
-        this._gameObject.transform.position = _board.GetNearestPosition(this._gameObject.transform.position);
+        transform.position = _board.GetNearestPosition(transform.position);
     }
 
     #region MonoBehaviour utilities
     // Use this for initialization
     void Start () {
         this._board = FindObjectOfType<Board>();
-        this._gameObject = this.GetComponent<GameObject>();
 	}
 	
 	// Update is called once per frame
