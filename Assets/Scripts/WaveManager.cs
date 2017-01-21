d3b6e742f4347df59cdcb98aae034723b6eff2e3
Aidan <aidan@digitgaming.com>
2017-01-21 13:40:40 +0000

    added basic wave counter

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveManager {
+
+    int _turnCounter = -1;
+    int _turnOfNextWave;
+
+    public WaveManager()
+    {
+        this.CalculateTurnOfNextWave();
+    }
+
+	public void ApplyWaveToBoard(Board board)
+    {
+        this._turnCounter++;
+        if (this._turnCounter >= this._turnOfNextWave)
+        {
+            // Do tha wave!
+            this._turnCounter = -1;
+            this.CalculateTurnOfNextWave();
+        }
+    }
+
+    private void CalculateTurnOfNextWave()
+    {
+        this._turnOfNextWave = Random.Range(1, 4);
+    }
+}
