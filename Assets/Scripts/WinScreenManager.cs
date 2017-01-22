776e9b41b0443f819c75621d23483555dd30ded4
stephen farrell <stephen farrell>
2017-01-22 15:27:18 +0000

    Win screen locked in portrains as well.

--- Assets/Scripts/WinScreenManager.cs
+++ Assets/Scripts/WinScreenManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class WinScreenManager : MonoBehaviour {
 
+    void Start() {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
     void Update() {
         if (Input.GetMouseButtonUp(0))
             SceneManager.LoadScene("MainMenu");
