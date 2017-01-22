c3212122411fe5a7b653b718b25b2c8397c26a57
Aidan <aidan@digitgaming.com>
2017-01-22 13:46:31 +0000

    derp:

---
+++ Assets/Scripts/WinScreenManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinScreenManager : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
